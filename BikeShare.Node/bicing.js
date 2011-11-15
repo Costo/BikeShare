e36f38ddf8a192bd849792553dc902ddebdf9096
Vincent Costel <vincent.costel@gmail.com>
2011-11-14 21:04:58 -0500

    added bicing nodejs script

---
+++ BikeShare.Node/bicing.js
@@ -0,0 +1,72 @@
+var http = require('http'),
+   vm = require('vm');
+
+var markers = [];
+var latlng;
+
+http.get({ host: 'www.bicing.cat', path: '/localizaciones/localizaciones.php'}, function(res) {
+   //console.log("statusCode: ", res.statusCode);
+   //console.log("headers: ", res.headers);
+   var data = [];
+   res.on('data', function(d) {
+      data.push(d);
+   });
+   res.on('end', function() {
+      var pattern =/<script[^>]*>([\s\S]*?)<\/script>/ig, 
+         m;
+      while (m = pattern.exec(data.join('')) ) {
+         if( m[1].indexOf( 'GBrowserIsCompatible' ) > -1 )
+         {
+            sandbox = {
+               GBrowserIsCompatible: function(){ return true; },
+               GLatLng: fakeGLatLong,
+               GIcon: function(){},
+               GSize: function(){},
+               GPoint: function(){},
+               GEvent: {
+                  addListener: function(){
+                     // execute handler immediately
+                     arguments[2]();
+                  }
+               },
+               GMarker: function(){},
+               GMap2: function(){
+                  this.addControl = function(){};
+                  this.enableScrollWheelZoom = function(){};
+                  this.setCenter = function(){};
+                  this.addOverlay = function(){};
+               },
+               GLargeMapControl: function(){},
+               GMapTypeControl: function(){},
+               document: {
+                  getElementById:function(){}
+               },
+               $: {
+                  ajax: fakeAjax
+               }
+            };
+            vm.runInNewContext(m[1], sandbox);
+            break;
+         }
+      }
+    
+      sandbox.load();
+      var strMarkers = [];
+      for(var i in markers) {
+         strMarkers[i] = JSON.stringify(markers[i]);
+      }
+      process.stdout.write('[' + strMarkers.join( ', ' ) + ']');
+   });
+}).on('error', function(e) {
+   console.error(e);
+});
+
+function fakeGLatLong(lat, lng)
+{
+   markers.push({ lat:lat, lng:lng });
+}
+
+function fakeAjax(options)
+{
+   markers[markers.length - 1].data = options.data;
+}
